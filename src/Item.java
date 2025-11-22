29b376dc9425e807bd44dc418490c36717f32551
Hugo Luis <hugolcabreu@gmail.com>
2025-11-21 21:09:10 -0300

    Criação classe item e lista de compras

---
+++ src/Item.java
@@ -0,0 +1,30 @@
+public class Item {
+    private String nome;
+    private int quantidade;
+
+    public Item(String nome, int quantidade){
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public String toString(){
+        return nome + " - " + quantidade;
+    }
+    
+}
---
+++ src/ListaDeCompras.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeCompras {
+    private List<Item> itens = new ArrayList<>();
+
+    public void adicionarItem(Item item){
+        itens.add(item);
+    }
+
+    public void removerItem(){
+        
+    }
+}
