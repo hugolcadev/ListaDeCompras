c875bd61d6c7df7f39a17c2b8f345f60d4de7b8d
Hugo Luis <hugolcabreu@gmail.com>
2025-11-21 22:19:45 -0300

    Implementação da classe ListaDeCompras e suporte de arquivos em json

--- src/ListaDeCompras.java
+++ src/ListaDeCompras.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+
+
 
 public class ListaDeCompras {
     private List<Item> itens = new ArrayList<>();
@@ -8,7 +16,31 @@ public class ListaDeCompras {
         itens.add(item);
     }
 
-    public void removerItem(){
-        
+    public void removerItem(String nome){
+        itens.removeIf(item -> item.getNome().equalsIgnoreCase(nome));
+    }
+
+    public List<Item> getItens(){
+        return itens;
+    }
+
+    public void listaItens(){
+        if (itens.isEmpty()){
+            System.out.println("A lista está vazia.");
+        }else{
+            for (int i=0; i<itens.size(); i++){
+                System.out.println(itens.get(i));
+            }
+        }
+    }
+
+    public void salvarEmJson(){
+        Gson gson = new Gson();
+        try (FileWriter writer = new FileWriter("lista.json")){
+            gson.toJson(itens, writer);
+            System.out.println("Lista salva com sucesso!");
+        }catch (Exception e){
+            System.out.println("Erro ao salvar em Json." + e.getMessage());
+        }
     }
 }
